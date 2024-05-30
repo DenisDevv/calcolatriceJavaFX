410b0ea468b5445ad01d5023636a43f7f08518f4
DenisDevv <karajedenis@gmail.com>
2024-05-30 11:51:36 +0200

    Creata funzione addizione

--- src/main/java/com/example/softwarejavafx/HelloController.java
+++ src/main/java/com/example/softwarejavafx/HelloController.java
@@ -5,16 +5,17 @@ import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField operando1;
     @FXML
-    private TextField testoUno;
+    private TextField operando2;
+    @FXML
+    private Label ris;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-    @FXML
-    protected void onButton() {
-        testoUno.setText("test");
+    protected void piu() {
+        Integer op1 = Integer.getInteger(operando1.getText());
+        Integer op2 = Integer.getInteger(operando2.getText());
+        int risultato = op1 + op2;
+        ris.setText("" + risultato);
     }
 }
